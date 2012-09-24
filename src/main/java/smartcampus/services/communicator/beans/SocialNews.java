f97ffd2466bacca1eea51feca55d52f38f58d414
adami <adami@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-09-24 09:15:00 +0000

    Updates for new proto.

--- src/main/java/smartcampus/services/communicator/beans/SocialNews.java
+++ src/main/java/smartcampus/services/communicator/beans/SocialNews.java
@@ -6,11 +6,19 @@ import eu.trentorise.smartcampus.services.social.data.message.Social.News;
 
 public class SocialNews implements Serializable {
 
+	private static final long serialVersionUID = -5589620882308016936L;
+	
 	private String topicName;
 	private Long topicId;
 	private String title;
 	private String entityType;
 	private Long entityId;
+	
+	private Long relatedId;
+	private String relatedTitle;
+	private String relatedEntityType;
+	
+	private boolean update;
 	
 	public SocialNews(News news) {
 		this.topicName = news.getTopicName();
@@ -18,6 +26,12 @@ public class SocialNews implements Serializable {
 		this.title = news.getTitle();
 		this.entityType = news.getEntityType();
 		this.entityId = news.getEntityId();
+		if (news.hasRelated()) {
+			this.relatedId = news.getRelated().getId();
+			this.relatedTitle = news.getRelated().getTitle();
+			this.relatedEntityType = news.getRelated().getEntityType();
+		}
+		this.update = news.getUpdate();
 	}
 
 	public String getTopicName() {
@@ -58,6 +72,38 @@ public class SocialNews implements Serializable {
 
 	public void setEntityId(Long entityId) {
 		this.entityId = entityId;
+	}
+
+	public Long getRelatedId() {
+		return relatedId;
+	}
+
+	public void setRelatedId(Long relatedId) {
+		this.relatedId = relatedId;
+	}
+
+	public String getRelatedTitle() {
+		return relatedTitle;
+	}
+
+	public void setRelatedTitle(String relatedTitle) {
+		this.relatedTitle = relatedTitle;
+	}
+
+	public String getRelatedEntityType() {
+		return relatedEntityType;
+	}
+
+	public void setRelatedEntityType(String relatedEntityType) {
+		this.relatedEntityType = relatedEntityType;
+	}
+
+	public boolean isUpdate() {
+		return update;
+	}
+
+	public void setUpdate(boolean update) {
+		this.update = update;
 	}
 	
 }
