f06e0809f3d83abcc2d10d1243b5748ca512861b
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-10-05 07:30:28 +0000

    monitor recurrecnt journey missing, added parameters.

--- src/main/java/smartcampus/services/communicator/helpers/JourneyPlannerHelper.java
+++ src/main/java/smartcampus/services/communicator/helpers/JourneyPlannerHelper.java
@@ -1,7 +1,6 @@
 package smartcampus.services.communicator.helpers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import smartcampus.services.communicator.beans.EntityObject;
@@ -15,7 +14,7 @@ import smartcampus.smartplanner.data.message.alerts.AlertStrike;
 
 public class JourneyPlannerHelper {
 
-	public static Notification[] buildNotification(Alert alert, String funnelId, JourneyPlannerParameters pars, String clientId) {
+	public static Notification[] buildNotification(Alert alert, String funnelId, JourneyPlannerParameters pars, String clientId, String name) {
 		List<Notification> result = new ArrayList<Notification>();
 
 		boolean all = true;
@@ -28,24 +27,24 @@ public class JourneyPlannerHelper {
 
 		if (alert instanceof AlertDelay) {
 			if (pars.getTypes().contains(JourneyPlannerParameters.DELAYS) || all) {
-				Notification not = buildNotification(alert, funnelId, "Delay", clientId);
+				Notification not = buildNotification(alert, funnelId, "Delay", clientId, name);
 				result.add(not);
 			}
 		} else if (alert instanceof AlertStrike) {
 			if (pars.getTypes().contains(JourneyPlannerParameters.STRIKES) || all) {
-				Notification not = buildNotification(alert, funnelId, "Strike", clientId);
+				Notification not = buildNotification(alert, funnelId, "Strike", clientId, name);
 				result.add(not);
 			}
 		} else if (alert instanceof AlertParking) {
 			if (pars.getTypes().contains(JourneyPlannerParameters.PARKINGS) || all) {
-				Notification not = buildNotification(alert, funnelId, "Parking", clientId);
+				Notification not = buildNotification(alert, funnelId, "Parking", clientId, name);
 				result.add(not);
 			}
 		}
 		return (Notification[]) result.toArray(new Notification[result.size()]);
 	}
 
-	private static Notification buildNotification(Alert alert, String funnelId, String title, String clientId) {
+	private static Notification buildNotification(Alert alert, String funnelId, String title, String clientId, String name) {
 		Notification not = new Notification();
 		not.setTitle(title + " Alert");
 		not.setDescription(alert.getNote());
@@ -55,6 +54,7 @@ public class JourneyPlannerHelper {
 		EntityObject eo = new EntityObject();
 		eo.setId(clientId);
 		eo.setType("journey");
+		eo.setTitle(name);
 		eos.add(eo);
 		
 		not.setEntities(eos);
